8f76b542f187bab33da2607fa055afee66ed9a6d
initsu <init.public@gmail.com>
2026-09-13 08:35:03 +0200

    Archive 5.2.0

--- archive_version.sh
+++ archive_version.sh
@@ -17,20 +17,6 @@ mkdir -p "$VERSION"
 
 # Move all the listed files and folders into that folder
 # Exclude the version folder itself to avoid recursion
-mv \
-   _framework \
-   Asm \
-   js65 \
-   Sprites \
-   app.css \
-   CrossPlatformUI.Browser.runtimeconfig.json \
-   PalaceRooms.json \
-   index.html \
-   ips-manifest.txt \
-   main.js \
-   package.json \
-   z2r-logo.png \
-   "$VERSION"/
 
 # Add to git and commit
 git add .
